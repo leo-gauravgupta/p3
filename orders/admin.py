95b89256dfe2931571bcf87f417cd6a254d6d4c0
leo-gauravgupta <leo.gauravgupta@gmail.com>
2018-04-19 04:51:55 -0500

    p3

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Menu, Customer, Order
+
+# to display all columns on the admin site:
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('item', 'sub_item', 'price_small', 'price_large')
+
+    def item(self, obj):
+        return obj.item
+
+    def sub_item(self, obj):
+        return obj.sub_item
+
+    def price_small(self, obj):
+        return obj.price_small
+
+    def price_large(self, obj):
+        return obj.price_large
+
+class CustomerAdmin(admin.ModelAdmin):
+    customer_display = ('first', 'last')
+
+    def first(self, obj):
+        return obj.first
+
+    def last(self, obj):
+        return obj.last
+
+class OrderAdmin(admin.ModelAdmin):
+#    order_display = ('order_item', 'order_customer', 'order_total')
+    order_display = ('item', 'sub_item', 'price_small', 'price_large', 'order_customer', 'order_total')
+
+    def item(self, obj):
+        return obj.item
+
+    def sub_item(self, obj):
+        return obj.sub_item
+
+    def price_small(self, obj):
+        return obj.price_small
+
+    def price_large(self, obj):
+        return obj.price_large
+
+    #def order_item(self, obj):
+    #    return obj.order_item.item
+
+    def order_customer(self, obj):
+        return obj.order_customer
+
+    def order_total(self, obj):
+        return obj.order_total
+
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Order, OrderAdmin)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    item = models.CharField(max_length=64, blank=True, null=True)
+    sub_item = models.CharField(max_length=64, blank=True, null=True)
+    price_small = models.FloatField(blank=True, null=True)
+    price_large = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.item} {self.sub_item} {self.price_small} {self.price_large}"
+
+class Customer(models.Model):
+    first = models.CharField(max_length=64, blank=True, null=True)
+    last = models.CharField(max_length=64, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
+
+#class Order(models.Model):
+class Order(Menu):
+    #order_item = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="orders").objects.get(item)
+    #order_item = models.Menu.item()
+    #item = Menu.item
+    #sub_item = Menu.sub_item
+    #price_small = Menu.price_small
+    #price_large = Menu.price_large
+    order_customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="customers_waiting", blank=True, null=True)
+    order_total = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        #return f"{self.id} - {self.order_item} for {self.order_customer} total {self.order_total}"
+        return f"{self.id} - {self.item} {self.sub_item} for {self.order_customer} total {self.order_total}"
